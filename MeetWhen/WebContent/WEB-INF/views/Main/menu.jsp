d8b4e9724708a59f875f810e5d2a42249577d867
min <onsway@naver.com>
2019-08-16 15:11:03 +0900

    GitHub-test

---
+++ MeetWhen/WebContent/WEB-INF/views/Main/menu.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+    
+    
+	<nav class="navbar navbar-expand  navbar-dark bg-dark">
+		<div class="container" >
+			<div class="navbar-header">
+				<a class="navbar-brand" href="./welcome.jsp">home</a>
+				<a class="navbar-brand" href="./signUp.jsp">| Sign-up</a>
+				<a class="navbar-brand" href="#">| Log-in</a>
+				<a class="navbar-brand" href="./products.jsp">| Product</a>
+				<a class="navbar-brand" href="./addProduct.jsp">| Add Product</a>
+				
+			</div>
+		</div>
+	</nav>
---
+++ MeetWhen/src/MeetWhen/spring/bean/HelloBean.java
@@ -0,0 +1,33 @@
+package MeetWhen.spring.bean;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Main/")
+public class HelloBean {
+	
+	@RequestMapping("welcome.mw")
+	public String welcome() {
+		System.out.println("Welcome Runing..!");
+		return "/Main/welcome";
+	}
+	
+	@RequestMapping("menu.mw")
+	public String menu() {
+		return "/Main/menu";
+	}
+	
+	@RequestMapping("footer.mw")
+	public String footer() {
+		return "/Main/footer";
+	}
+	/*--------------------------------------*/
+	
+	@RequestMapping("googlemaps.mw")
+	public String googlemaps() {
+		return "/Main/googlemaps";
+	}
+
+
+}
