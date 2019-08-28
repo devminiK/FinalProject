b3e05d4debe568ebc422cd5e52014c9160984b97
onsway <onsway@naver.com>
2019-08-28 17:45:57 +0900

    네이버 로그인 연동

--- MeetWhen/src/MeetWhen/spring/bean/LogBean.java
+++ MeetWhen/src/MeetWhen/spring/bean/LogBean.java
@@ -1,35 +1,18 @@
 package MeetWhen.spring.bean;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 //로그인 관련
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/Log/")
+@RequestMapping(value="/Log/")
 public class LogBean {
 	@RequestMapping("loginForm.mw")
-	public String loginForm(HttpSession session) {
+	public String loginForm() {
+	
 		return "/Log/loginForm";
 	}
-	
-	//네이버 로그인 연동 도전.
-	@RequestMapping("naverlogin.mw")
-	public String naverlogin(HttpSession session) {
-		//네이버 로그인 추가
-		
-		return "/Log/naverlogin";
-	}
-	@RequestMapping("callback.mw")
-	public String callback(HttpServletRequest request) throws Exception {
-		//네이버 로그인 추가
-		
-		return "/Log/callback";
-	}
-	
-	
+
 	@RequestMapping("loginPro.mw")
 	public String loginPro() {
 		
